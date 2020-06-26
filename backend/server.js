bca3c774da5c69de336b52d19357151c9ec1d014
majimmy88 <majimmy88@gmail.com>
2020-06-25 20:26:18 -0700

    Server initialized

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT||5500;
+
+app.use(cors());
+
+app.use(express.json());
+
+const uri = process.env.ALTAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+
+const connection = mongoose.connection;
+
+connection.once('open', ()=> {
+    console.log("MongoDB database connection established successfully");
+})
+
+app.listen(port, () => {
+    console.log(`Sever is running on port: ${port}`)
+});
