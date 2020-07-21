c3035c374bb1893647f4d49f9bccfe2cb2cc3f85
majimmy88 <majimmy88@gmail.com>
2020-07-21 10:55:53 -0700

    Cleaned up map component

--- src/components/map.component.js
+++ src/components/map.component.js
@@ -5,13 +5,6 @@ import GoogleMapReact from 'google-map-react';
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 class Map extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state ={
-  //     key: '',
-  //   }
-  // }
-
   static defaultProps = {
     center: {
       lat: 37.77,
@@ -19,17 +12,6 @@ class Map extends Component {
     },
     zoom: 13
   };
-
-  // componentDidMount() {
-  //   axios.get('http://localhost:5500/map/')
-  //     .then(response =>{
-  //       this.setState({ key:response.data })
-  //     })
-  //     .catch(error=>{
-  //       console.log(error)
-  //     })
-  // }
-
 
   render() {
     return (
