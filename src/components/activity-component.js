1befc220c44aea660c20d2b09e6e47b5967ec96e
majimmy88 <majimmy88@gmail.com>
2020-08-04 23:54:09 -0700

    Updated activity component

--- src/components/activity-component.js
+++ src/components/activity-component.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Activity extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            activity: {},
-        }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         activity: {},
+    //     }
 
-    }
+    // }
     render() {
         return (
             <tr>
