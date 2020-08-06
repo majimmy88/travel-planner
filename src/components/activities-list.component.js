6240806a25b5d318deac1dcc3ffb98973cae0dc5
majimmy88 <majimmy88@gmail.com>
2020-08-05 21:13:35 -0700

    Fixed indents

--- src/components/activities-list.component.js
+++ src/components/activities-list.component.js
@@ -3,62 +3,59 @@ import axios from 'axios';
 import Activity from './activity-component'
 
 export default class ActivitiesList extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {activities: []};
+        this.state = {activities: []};
 
-    this.deleteActivity = this.deleteActivity.bind(this)
-  }
+        this.deleteActivity = this.deleteActivity.bind(this)
+    }
 
-  componentDidMount() {
-    axios.get('http://localhost:5500/activities/')
-      .then(response => {
-        this.setState({ activities: response.data })
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-  }
+    componentDidMount() {
+        axios.get('http://localhost:5500/activities/')
+        .then(response => {
+            this.setState({ activities: response.data })
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
 
-  deleteActivity(id) {
-    axios.delete('http://localhost:5500/activities/'+id)
-      .then(response => { console.log(response.data)});
+    deleteActivity(id) {
+        axios.delete('http://localhost:5500/activities/'+id)
+        .then(response => { console.log(response.data)});
 
-    this.setState({
-      exercises: this.state.activities.filter(el => el._id !== id)
-    })
-  }
+        this.setState({
+        exercises: this.state.activities.filter(el => el._id !== id)
+        })
+    }
 
-  activityList() {
-    return this.state.activities.map(currentactivity => {
-      return <Activity activity={currentactivity} deleteActivity={this.deleteActivity} key={currentactivity._id}/>;
-    })
-  }
+    activityList() {
+        return this.state.activities.map(currentactivity => {
+        return <Activity activity={currentactivity} deleteActivity={this.deleteActivity} key={currentactivity._id}/>;
+        })
+    }
 
-  render() {
-    return (
-      <div>
-        <h3>Logged Activities</h3>
-        <table className="table">
-          <thead className="thead-light">
-            <tr>
-              <th>Location</th>
-                <button type="button">
-                Name
-                </button>
-              <th>Description</th>
-              <th>Duration</th>
-              <th>Date</th>
-              <th>Username</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            { this.activityList() }
-          </tbody>
-        </table>
-      </div>
-    )
-  }
+    render() {
+        return (
+        <div>
+            <h3>Logged Activities</h3>
+            <table className="table">
+                <thead className="thead-light">
+                    <tr>
+                        <th>Location</th>
+                        <th>Description</th>
+                        <th>Duration</th>
+                        <th>Date</th>
+                        <th>Username</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { this.activityList() }
+                </tbody>
+            </table>
+        </div>
+        )
+    }
 }
--- src/components/activity-component.js
+++ src/components/activity-component.js
@@ -2,25 +2,25 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Activity extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         activity: {},
-    //     }
+    constructor(props) {
+        super(props);
+        this.state = {
+            activity: {},
+        }
 
-    // }
+    }
     render() {
         return (
             <tr>
-            <td>{this.props.activity.location}</td>
-            <td>{this.props.activity.description}</td>
-            <td>{this.props.activity.duration}</td>
-            <td>{this.props.activity.date.substring(0,10)}</td>
-            <td>{this.props.activity.username}</td>
-            <td>
-              <Link to={"/edit/"+this.props.activity._id}>edit</Link> | <a href="#" onClick={() => { this.props.deleteActivity(this.props.activity._id) }}>delete</a>
-            </td>
-          </tr>
+                <td>{this.props.activity.location}</td>
+                <td>{this.props.activity.description}</td>
+                <td>{this.props.activity.duration}</td>
+                <td>{this.props.activity.date.substring(0,10)}</td>
+                <td>{this.props.activity.username}</td>
+                <td>
+                <Link to={"/edit/"+this.props.activity._id}>edit</Link> | <a href="#" onClick={() => { this.props.deleteActivity(this.props.activity._id) }}>delete</a>
+                </td>
+            </tr>
         )
     }
 }
